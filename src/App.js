9716e597afd2f2fc1b107973f14a6bfcdb3469e2
Suvradeep <suvradeep.paria829@gmail.com>
2020-09-13 21:29:34 +0530

    structure push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+const API_KEY = 'f0d7e24a3286431eba49454874866fe2';
+const [ news, setNews ] = useState([]);
+
+const getNews = () => {
+  axios.get(`http://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`)
+  .then( response => {
+    setNews(response.data.articles)
+    console.log(response.data)
+  })
+  .catch( error => {
+    console.log(error)
+  })
+}
+useEffect(() => {
+  getNews();
+},[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header ">News</div>
+    {news.map((data, index) => (  
+      <h5 key={index}>{data.title}</h5>
+    ))}
     </div>
   );
 }
