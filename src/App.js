7dc410d00e1c157fde81fee41dd56f302c51d542
Suvradeep <suvradeep.paria829@gmail.com>
2020-09-14 14:18:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const API_KEY = 'f0d7e24a3286431eba49454874866fe2';
 const [ news, setNews ] = useState([]);
 
 const getNews = () => {
-  axios.get(`http://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`)
+  axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`)
   .then( response => {
     setNews(response.data.articles)
   })
