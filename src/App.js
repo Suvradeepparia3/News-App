3a45f93e4fba6c22aa7372a1f8ca2276a1899476
Suvradeep <suvradeep.paria829@gmail.com>
2020-09-15 23:16:57 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -10,11 +10,65 @@ const [ news, setNews ] = useState([]);
 
 
 const getNews = () => {
-  axios.get(`http://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`)
-  .then( response => {
-    setNews(response.data.articles)
+  // axios.get(`http://newsapi.org/v2/top-headlines?country=in&apiKey=${API_KEY}`)
+  // .then( response => {
+  //   setNews(response.data.articles)
+  // })
+  // .catch( error => {
+  //   console.log(error)
+  // })
+  // fetch("https://newscatcher.p.rapidapi.com/v1/latest_headlines?lang=en&media=True", {
+  //   "method": "GET",
+  //   "headers": {
+  //     "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+  //     "x-rapidapi-key": "8680f631a1mshaa755e93cbd7de4p16905djsn204d8a614eb6"
+  //   }
+  // })
+  // .then(response => {
+  //   console.log(response.articles);
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
+// axios({
+//     "method":"GET",
+//     "url":"https://newscatcher.p.rapidapi.com/v1/latest_headlines",
+//     "headers":{
+//     "content-type":"application/octet-stream",
+//     "x-rapidapi-host":"newscatcher.p.rapidapi.com",
+//     "x-rapidapi-key":"8680f631a1mshaa755e93cbd7de4p16905djsn204d8a614eb6",
+//     "useQueryString":true
+//     },"params":{
+//     "lang":"en",
+//     "media":"True"
+//     }
+//     })
+//     .then((response)=>{
+//       setNews(response.data.articles)
+//       console.log(response.data.articles)
+//     })
+//     .catch((error)=>{
+//       console.log(error)
+//     })
+axios({
+  "method":"GET",
+  "url":"https://bing-news-search1.p.rapidapi.com/news",
+  "headers":{
+  "content-type":"application/octet-stream",
+  "x-rapidapi-host":"bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key":"8680f631a1mshaa755e93cbd7de4p16905djsn204d8a614eb6",
+  "x-bingapis-sdk":"true",
+  "useQueryString":true
+  },"params":{
+  "safeSearch":"Off",
+  "textFormat":"Raw"
+  }
   })
-  .catch( error => {
+  .then((response)=>{
+    setNews(response.data.value)
+    console.log(response)
+  })
+  .catch((error)=>{
     console.log(error)
   })
 }
--- src/NewsPlate.js
+++ src/NewsPlate.js
@@ -6,14 +6,15 @@ function NewsPlate(props) {
     return (
         <div className="container">
             <section className="plate">
-            <img  className="imageDiv" src={props.data.urlToImage} alt="responsive" />
+            <img  className="imageDiv" src={props.data.image.thumbnail.contentUrl} alt="responsive" />
             
             <div className="info">
-            Published: <b>{props.data.publishedAt.slice(0, 10)}</b><br />
-            Source: <b>{props.data.source.name}</b> 
+            Published: <b>{props.data.datePublished.slice(0, 10)}</b><br />
+            Source: <b>{props.data.provider.map((name,index) => 
+                        (<span key={index}>{name.name}</span>))}</b> 
              
             <div className="title">
-            <b>{props.data.title}</b>
+            <b>{props.data.name}</b>
             </div>
             
             <div className="description">
